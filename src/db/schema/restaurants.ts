4fb86f8e2562f70a515dcda8ff15e647124c752d
Jhone-cmd <jhone.1941@gmail.com>
2025-09-17 15:04:53 -0300

    feat: created the schema of the restaurants table

---
+++ src/db/schema/restaurants.ts
@@ -0,0 +1,16 @@
+import { createId as cuid } from "@paralleldrive/cuid2"
+import { pgTable, text, timestamp } from "drizzle-orm/pg-core"
+import { users } from "./users"
+
+export const restaurants = pgTable("restaurants", {
+  id: text("id")
+    .$defaultFn(() => cuid())
+    .primaryKey(),
+  name: text("name").notNull(),
+  description: text("description"),
+  managerId: text("manager_id").references(() => users.id, {
+    onDelete: "set null",
+  }),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+})
