f9386113dcf07b0dcfa2a868629b52a6e60e0d21
Jhone-cmd <jhone.1941@gmail.com>
2025-09-18 09:23:34 -0300

    feat: created seed for the database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { env } from "./src/env/schema"
 
 export default defineConfig({
   dialect: "postgresql",
-  schema: "./src/db/schema/index.ts",
+  schema: "./src/db/schemas/index.ts",
   out: "./drizzle",
   dbCredentials: {
     url: env.DATABASE_URL,
---
+++ src/db/seed.ts
@@ -0,0 +1,67 @@
+import { faker } from "@faker-js/faker"
+import chalk from "chalk"
+import { db } from "./connection"
+import { restaurants, users } from "./schemas"
+
+/**
+ * Reset Database
+ */
+
+await db.delete(users)
+await db.delete(restaurants)
+
+console.log(chalk.yellow("✔ Database reset!"))
+
+/**
+ * Create customers
+ */
+
+await db.insert(users).values([
+  {
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+  },
+  {
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+  },
+])
+
+console.log(chalk.yellow("✔ Create customers!"))
+
+/**
+ * Create manager
+ */
+
+const [manager] = await db
+  .insert(users)
+  .values([
+    {
+      name: faker.person.fullName(),
+      email: "admin@admin.com",
+      role: "manager",
+    },
+  ])
+  .returning({
+    id: users.id,
+  })
+
+console.log(chalk.yellow("✔ Create manager!"))
+
+/**
+ * Create restaurant
+ */
+
+await db.insert(restaurants).values([
+  {
+    name: faker.company.name(),
+    description: faker.lorem.paragraph(),
+    managerId: manager?.id,
+  },
+])
+
+console.log(chalk.yellow("✔ Create restaurant!"))
+
+console.log(chalk.greenBright("✔ Database seeded successfully"))
+
+process.exit()
