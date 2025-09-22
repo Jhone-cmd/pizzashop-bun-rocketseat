ecae4b3039b03f3e7e834bf2579a3dc9dbb8de92
Jhone-cmd <jhone.1941@gmail.com>
2025-09-22 09:50:54 -0300

    feat: JWT authentication setup created

--- src/env/schema.ts
+++ src/env/schema.ts
@@ -5,6 +5,8 @@ const envSchema = z.object({
   AUTH_REDIRECT_URL: z.url(),
   PORT: z.coerce.number().default(3333),
   DATABASE_URL: z.url(),
+  JWT_PRIVATE_KEY: z.string(),
+  JWT_PUBLIC_KEY: z.string(),
 })
 
 export const env = envSchema.parse(process.env)
--- src/http/app.ts
+++ src/http/app.ts
@@ -1,7 +1,25 @@
-import { Elysia } from "elysia"
+import jwt from "@elysiajs/jwt"
+import { Elysia, t } from "elysia"
+import { env } from "../env/schema"
 import { registerRestaurant } from "./routes/register-restaurant"
 import { sendAuthLink } from "./routes/send-auth-link"
 
 export const app = new Elysia()
+
+app.use(
+  jwt({
+    name: "jwt",
+    secret: {
+      priv: env.JWT_PRIVATE_KEY,
+      pub: env.JWT_PUBLIC_KEY,
+    },
+    alg: "RS256",
+    schema: t.Object({
+      sub: t.String(),
+      restaurantId: t.Optional(t.String()),
+    }),
+  })
+)
+
 app.use(registerRestaurant)
 app.use(sendAuthLink)
