c119bb4d81ec4c5c3778509dd542b5722d0f751f
Jhone-cmd <jhone.1941@gmail.com>
2025-09-22 09:15:49 -0300

    feat: testing route: sending the authentication link

--- src/http/app.ts
+++ src/http/app.ts
@@ -1,5 +1,7 @@
 import { Elysia } from "elysia"
 import { registerRestaurant } from "./routes/register-restaurant"
+import { sendAuthLink } from "./routes/send-auth-link"
 
 export const app = new Elysia()
 app.use(registerRestaurant)
+app.use(sendAuthLink)
