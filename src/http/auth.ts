1602dec65844ae8f883f5a48ef6f4b852deecd12
Jhone-cmd <jhone.1941@gmail.com>
2025-09-23 09:54:55 -0300

    feat: adjusting jwt for RS256 algorithm and testing

--- src/http/auth.ts
+++ src/http/auth.ts
@@ -1,13 +1,21 @@
+import crypto from "node:crypto"
 import jwt from "@elysiajs/jwt"
 import Elysia, { t } from "elysia"
 import { env } from "../env/schema"
 
+const keyBuffer = Buffer.from(env.JWT_PRIVATE_KEY, "base64")
+
+// 2. Cria o KeyObject a partir do buffer
+const privateKey = crypto.createPrivateKey({
+  key: keyBuffer,
+  format: "der", // Use "der" para chaves em formato binário (decodificadas de Base64)
+  type: "pkcs8", // O tipo de chave (Private Key)
+})
+
 export const auth = new Elysia().use(
   jwt({
-    secret: {
-      priv: env.JWT_PRIVATE_KEY,
-      pub: env.JWT_PUBLIC_KEY,
-    },
+    secret: privateKey, // Use the KeyObject directly
+    alg: "RS256",
     schema: t.Object({
       sub: t.String(),
       restaurantId: t.Optional(t.String()),
