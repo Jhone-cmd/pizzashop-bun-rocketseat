147ccd20f27af1c9bfdbd1a43de0f0a8113f2b63
Jhone-cmd <jhone.1941@gmail.com>
2025-09-23 10:39:03 -0300

    feat: created Route: logged in user profile

--- src/http/app.ts
+++ src/http/app.ts
@@ -1,10 +1,14 @@
 import { Elysia } from "elysia"
 import { authenticateFromLink } from "./routes/authenticate-from-link"
+import { getProfile } from "./routes/get-profile"
 import { registerRestaurant } from "./routes/register-restaurant"
 import { sendAuthLink } from "./routes/send-auth-link"
+import { singOut } from "./routes/sing-out"
 
 export const app = new Elysia()
 
 app.use(registerRestaurant)
 app.use(sendAuthLink)
 app.use(authenticateFromLink)
+app.use(singOut)
+app.use(getProfile)
--- src/http/auth.ts
+++ src/http/auth.ts
@@ -25,21 +25,47 @@ export const auth = new Elysia()
       schema: jwtPayload,
     })
   )
-  .derive({ as: "scoped" }, ({ jwt: { sign }, cookie: { authCookie } }) => {
-    return {
-      singIn: async (payload: Static<typeof jwtPayload>) => {
-        const token = await sign(payload)
+  .derive(
+    { as: "scoped" },
+    ({ jwt: { sign, verify }, cookie: { authCookie } }) => {
+      return {
+        singIn: async (payload: Static<typeof jwtPayload>) => {
+          const token = await sign(payload)
 
-        authCookie?.set({
-          value: token,
-          httpOnly: true,
-          maxAge: 60 * 60 * 24 * 7, // 7 days,
-          path: "/",
-        })
-      },
+          authCookie?.set({
+            value: token,
+            httpOnly: true,
+            maxAge: 60 * 60 * 24 * 7, // 7 days,
+            path: "/",
+          })
+        },
 
-      singOut: async () => {
-        authCookie?.remove()
-      },
+        singOut: async () => {
+          authCookie?.remove()
+        },
+
+        getCurrentUser: async () => {
+          if (!authCookie?.value) {
+            throw new Error("No token found in cookie")
+          }
+
+          try {
+            // Verify the token using the privateKey (public verification in RS256)
+            const payload = await verify(authCookie.value as string)
+
+            if (!payload) {
+              throw new Error("Token verification failed")
+            }
+
+            return {
+              userId: payload.sub,
+              restaurantId: payload.restaurantId,
+            }
+          } catch (error) {
+            console.error("Error verifying token:", error)
+            throw new Error("Unauthorized")
+          }
+        },
+      }
     }
-  })
+  )
