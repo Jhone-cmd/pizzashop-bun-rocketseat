dbb8656f90a84011ca205819b1c01b9b669009d0
Jhone-cmd <jhone.1941@gmail.com>
2025-09-24 08:58:22 -0300

    feat: created Route: managed restaurant

--- src/http/app.ts
+++ src/http/app.ts
@@ -1,5 +1,6 @@
 import { Elysia } from "elysia"
 import { authenticateFromLink } from "./routes/authenticate-from-link"
+import { getManagedRestaurant } from "./routes/get-managed-restaurant"
 import { getProfile } from "./routes/get-profile"
 import { registerRestaurant } from "./routes/register-restaurant"
 import { sendAuthLink } from "./routes/send-auth-link"
@@ -12,3 +13,4 @@ app.use(sendAuthLink)
 app.use(authenticateFromLink)
 app.use(singOut)
 app.use(getProfile)
+app.use(getManagedRestaurant)
