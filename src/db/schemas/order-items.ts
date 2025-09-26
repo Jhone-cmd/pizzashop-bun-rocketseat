7471c6c6bc2e68a45f8629c61175e30f1ab7646e
Jhone-cmd <jhone.1941@gmail.com>
2025-09-26 09:41:25 -0300

    feat: setting up orders_items table relationships in drizzle

--- src/db/schemas/order-items.ts
+++ src/db/schemas/order-items.ts
@@ -1,4 +1,5 @@
 import { createId as cuid } from "@paralleldrive/cuid2"
+import { relations } from "drizzle-orm"
 import { integer, pgTable, text } from "drizzle-orm/pg-core"
 import { orders, products } from "."
 
@@ -16,4 +17,19 @@ export const orderItems = pgTable("order_items", {
   }),
   priceInCents: integer("price_in_cents").notNull(),
   quantity: integer("quantity").notNull(),
+})
+
+export const ordersItemsRelations = relations(orderItems, ({ one }) => {
+  return {
+    product: one(products, {
+      fields: [orderItems.productId],
+      references: [products.id],
+      relationName: "order_items_product",
+    }),
+    order: one(orders, {
+      fields: [orderItems.orderId],
+      references: [orders.id],
+      relationName: "order_items_order",
+    }),
+  }
 })
