423f32f3eedb9c6a42a069dc64829ac6f0e7c03f
Jhone-cmd <jhone.1941@gmail.com>
2025-09-30 08:51:47 -0300

    feat: created data in the products table in the seed

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -27,16 +27,19 @@ console.log(chalk.yellow("✔ Database reset!"))
  * Create customers
  */
 
-await db.insert(users).values([
-  {
-    name: faker.person.fullName(),
-    email: faker.internet.email(),
-  },
-  {
-    name: faker.person.fullName(),
-    email: faker.internet.email(),
-  },
-])
+const [customer1, customer2] = await db
+  .insert(users)
+  .values([
+    {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+    },
+    {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+    },
+  ])
+  .returning()
 
 console.log(chalk.yellow("✔ Create customers!"))
 
@@ -63,15 +66,45 @@ console.log(chalk.yellow("✔ Create manager!"))
  * Create restaurant
  */
 
-await db.insert(restaurants).values([
-  {
-    name: faker.company.name(),
-    description: faker.lorem.paragraph(),
-    managerId: manager?.id,
-  },
-])
+const [restaurant] = await db
+  .insert(restaurants)
+  .values([
+    {
+      name: faker.company.name(),
+      description: faker.lorem.paragraph(),
+      managerId: manager!.id,
+    },
+  ])
+  .returning()
 
 console.log(chalk.yellow("✔ Create restaurant!"))
+
+/**
+ * Create products
+ */
+
+function generateProduct() {
+  return {
+    name: faker.commerce.productName(),
+    description: faker.commerce.productDescription(),
+    priceInCents: Number(faker.commerce.price({ min: 100, max: 500, dec: 0 })),
+    restaurantId: restaurant!.id,
+  }
+}
+
+const availableProducts = await db
+  .insert(products)
+  .values([
+    generateProduct(),
+    generateProduct(),
+    generateProduct(),
+    generateProduct(),
+    generateProduct(),
+    generateProduct(),
+  ])
+  .returning()
+
+console.log(chalk.yellow("✔ Create products!"))
 
 console.log(chalk.greenBright("✔ Database seeded successfully"))
 
